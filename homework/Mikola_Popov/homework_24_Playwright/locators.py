c80cee2edadb9657a288353613000c02005170f0
MikoJIa <kolya_popov_86@tut.by>
2026-04-27 21:56:13 +0300

    popov_nikolay_homework_24

--- homework/Mikola_Popov/homework_24_Playwright/locators.py
+++ homework/Mikola_Popov/homework_24_Playwright/locators.py
@@ -1,8 +1,12 @@
-loc_state = ("//div[@id='state']"
-             "/div[@class='css-13cymwt-control']"
-             "/div[@class='css-1wy0on6']"
-             "/div[@class='css-1xc3v61-indicatorContainer']")
-loc_city = ("//div[@id='city']"
-            "/div[@class='css-13cymwt-control']"
-            "/div[@class='css-1wy0on6']"
-            "/div[@class='css-1xc3v61-indicatorContainer']")
+loc_state = (
+    "//div[@id='state']"
+    "/div[@class='css-13cymwt-control']"
+    "/div[@class='css-1wy0on6']"
+    "/div[@class='css-1xc3v61-indicatorContainer']"
+)
+loc_city = (
+    "//div[@id='city']"
+    "/div[@class='css-13cymwt-control']"
+    "/div[@class='css-1wy0on6']"
+    "/div[@class='css-1xc3v61-indicatorContainer']"
+)
--- homework/Mikola_Popov/homework_24_Playwright/test_start_playwright.py
+++ homework/Mikola_Popov/homework_24_Playwright/test_start_playwright.py
@@ -1,28 +1,31 @@
 import time
-from playwright.sync_api import Page, expect
+from playwright.sync_api import Page
 
 
 def test_by_role(page: Page):
     page.goto("https://the-internet.herokuapp.com/")
     time.sleep(1)
-    #page.get_by_text('Form Authentication').click() # альтернативный поиск элемента
-    page.get_by_role('link', name='Form Authentication').click()
+    # page.get_by_text('Form Authentication').click()
+    # альтернативный поиск элемента
+    page.get_by_role("link", name="Form Authentication").click()
     time.sleep(1)
     username = page.locator("input[name='username']")
-    username.fill('Nikolay')
+    username.fill("Nikolay")
     time.sleep(1)
     password = page.locator("input[name='password']")
-    password.fill('80292788064np')
+    password.fill("80292788064np")
     time.sleep(1)
-    #page.locator('button[type="submit"]').click() # альтернативный поиск элемента
-    page.get_by_role('button').click()
+    # page.locator('button[type="submit"]').click()
+    # альтернативный поиск элемента
+    page.get_by_role("button").click()
     time.sleep(1)
+
 
 def test_search_element_by_text(page: Page):
     page.goto(" https://the-internet.herokuapp.com/")
     page.get_by_text("Form Authentication").click()
     username = page.get_by_label("username")
-    username.fill('Nikolay')
+    username.fill("Nikolay")
     password = page.get_by_label("password")
-    password.fill('80292788064np')
+    password.fill("80292788064np")
     page.get_by_role("button", name="Login").click()
